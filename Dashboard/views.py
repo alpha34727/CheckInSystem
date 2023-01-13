7ede4c3b9a9a9caca0f4f4efbaf9ddcbfc34c140
ourclasshomework <ourclasshomework@gmail.com>
2023-01-13 17:30:46 +0800

    0113 backup

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from .form import PromiseForm
 from .models import Member, Attendant
 
 # Create your views here.
 
-class Homepage(LoginRequiredMixin, ListView):
+class Homepage(ListView):
     model = Member
     login_url = reverse_lazy('login')
+
+    def get_context_data(self):
+        content = super().get_context_data()
+        content['name'] = User.objects.get(id=1)
+        
+        return content
 
     template_name = 'homepage.html'
     context_object_name = 'member'
